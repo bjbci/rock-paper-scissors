8b1e3c4423ef2aaebac16f1f8ee7fa73655a9686
Brian Bulowicz <bajebi.bb@gmail.com>
2022-09-12 19:37:25 -0500

    pseudo code

--- game.js
+++ game.js
@@ -1,1 +1,56 @@
-console.log("yo")
+//pseudocode
+
+// variable array for r = rock  p = paper s=scissors [r,p,s]
+// variable computerChoice
+// variable for playerChoice
+
+//variable wins
+// var losses
+// var ties
+
+//bigbox
+
+
+//smallerbox
+
+//text for instructions
+
+
+//prompt for r/p/s  prompt brings up popup box
+
+//randomly choose computer choice
+
+//entry box for r/p/s
+
+//compare choices
+
+//if r===r = tie
+//if p===p = tie
+//if s===s = tie
+//if r && s = r wins
+//if r && p= p wins
+//if p && s = s wins
+//if p && r = r wins
+//if s&& p = p wins
+//if s && r = r wins
+
+if(computerChoice[r] && playerChoice[r])
+if(computerChoice[p] && playerChoice[p])
+if(computerChoice[s] && playerChoice[s])
+if(computerChoice[r] && playerChoice[s])
+if(computerChoice[r] && playerChoice[p])
+if(computerChoice[p] && playerChoice[s])
+if(computerChoice[p] && playerChoice[r])
+if(computerChoice[s] && playerChoice[p])
+if(computerChoice[s] && playerChoice[r])
+
+
+//display results won, loss or tie array [win, lose,tie]
+
+//shows result history wins, losses, ties
+
+//ask to play again?
+
+//text play again
+//if yes-restart game
+//if no end game
